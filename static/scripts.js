ad88d0ac4d0353814660a263c7b18a2fa36ff1ed
Thainasli <thainaslim@gmail.com>
2023-11-10 14:14:04 -0300

    Atualizações do commit

--- static/scripts.js
+++ static/scripts.js
@@ -3,18 +3,15 @@ function calcularEmprestimo() {
     var valor_emprestimo = parseFloat(document.getElementById('valor_emprestimo').value);
     var meses_pagamento = parseInt(document.getElementById('meses_pagamento').value);
 
-    // Sua lógica de cálculo aqui
     var taxa_juros_mensal = 0.03;
     var taxa_juros_porcentagem = taxa_juros_mensal * 100;
     var juros_cobrado = valor_emprestimo * taxa_juros_mensal * meses_pagamento;
     var prestacao_mensal = (valor_emprestimo + juros_cobrado) / meses_pagamento;
     var custo_total = valor_emprestimo + juros_cobrado;
 
-    // Atualize os elementos na página com os resultados
     document.getElementById('prestacao-mensal').textContent = prestacao_mensal.toFixed(2);
     document.getElementById('custo-total').textContent = custo_total.toFixed(2);
     document.getElementById('taxa-juros').textContent = taxa_juros_porcentagem.toFixed(2) + "%";
 
-    // Exiba o contêiner de resultados
     document.getElementById('result-container').style.display = 'block';
 }
